1a95c492b82a720ca28bcbeedf1b5fbdf6531b7a
Youfloww <yuvrajjakharyt@gmail.com>
2025-08-30 21:51:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics.js";
 
 // ===================================================================================
 // FIREBASE INITIALIZATION
@@ -20,7 +21,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-
+const analytics = getAnalytics(app);
 // Global reference to the current user's data
 let currentUserData = {};
 let userDataRef = null;
